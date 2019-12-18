a006b06c0ed3212352904402e884926e36277b07
Mitchell Kerr <mkerr@vailsys.com>
2019-12-18 14:46:31 -0600

    Updates

---
+++ precache-manifest.757638c507e6678f5b4f8da8d3199561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c07e709ef032c6084402af1f11a4e3",
+    "url": "/mrkerr/react-music/index.html"
+  },
+  {
+    "revision": "129d6a2e4a51e7e80ea7",
+    "url": "/mrkerr/react-music/static/css/main.ddf3530b.chunk.css"
+  },
+  {
+    "revision": "412e6dc3e25a77a4db36",
+    "url": "/mrkerr/react-music/static/js/2.3a50d116.chunk.js"
+  },
+  {
+    "revision": "e216c5ff72dc43bf6f3aac30b1c2fbe3",
+    "url": "/mrkerr/react-music/static/js/2.3a50d116.chunk.js.LICENSE"
+  },
+  {
+    "revision": "129d6a2e4a51e7e80ea7",
+    "url": "/mrkerr/react-music/static/js/main.9cb9b8d3.chunk.js"
+  },
+  {
+    "revision": "ed88857f502ab572cc1f",
+    "url": "/mrkerr/react-music/static/js/runtime-main.3d9a8ea7.js"
+  },
+  {
+    "revision": "aa21c76c2f69863c3001bdd5962a8732",
+    "url": "/mrkerr/react-music/static/media/output-onlinejpgtools.aa21c76c.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mrkerr/react-music/precache-manifest.757638c507e6678f5b4f8da8d3199561.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mrkerr/react-music/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
