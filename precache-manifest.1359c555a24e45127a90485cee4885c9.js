fe0674bbb007877a048566159afe91ff4f1e3972
Mitchell Kerr <mkerr@vailsys.com>
2019-12-21 19:06:40 -0600

    Updates

---
+++ precache-manifest.1359c555a24e45127a90485cee4885c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b26115dec2ddb53e27ff19c1ba5309c",
+    "url": "/react-music/index.html"
+  },
+  {
+    "revision": "307e82d2d3fbc3914297",
+    "url": "/react-music/static/css/main.fdec8ac2.chunk.css"
+  },
+  {
+    "revision": "44ecbddf44a42157822e",
+    "url": "/react-music/static/js/2.12b4546a.chunk.js"
+  },
+  {
+    "revision": "e216c5ff72dc43bf6f3aac30b1c2fbe3",
+    "url": "/react-music/static/js/2.12b4546a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "307e82d2d3fbc3914297",
+    "url": "/react-music/static/js/main.e622e4e4.chunk.js"
+  },
+  {
+    "revision": "a8824d61152e10ce697f",
+    "url": "/react-music/static/js/runtime-main.2e1c8f71.js"
+  },
+  {
+    "revision": "aa21c76c2f69863c3001bdd5962a8732",
+    "url": "/react-music/static/media/output-onlinejpgtools.aa21c76c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b034a1cd0c0cdf11bf96d14f2989a6e.js"
+  "/react-music/precache-manifest.1359c555a24e45127a90485cee4885c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-music/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
